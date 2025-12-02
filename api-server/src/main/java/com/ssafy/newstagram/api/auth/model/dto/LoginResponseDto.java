c01679cdf735dc5730ba03858ccfb094bc8f27dd
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-12-02 16:51:23 +0900

    [feat] - 로그인 응답 dto 수정

--- api-server/src/main/java/com/ssafy/newstagram/api/auth/model/dto/LoginResponseDto.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/auth/model/dto/LoginResponseDto.java
@@ -7,5 +7,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public class LoginResponseDto {
     private String accessToken;
-    private String tokenType = "Bearer";
+    private String refreshToken;
+//    private String tokenType = "Bearer";
 }
