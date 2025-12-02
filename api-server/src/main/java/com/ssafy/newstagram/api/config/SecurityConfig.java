d4e53f2bd87a73331a20b579b8b5c5daa88860d3
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-12-02 15:30:30 +0900

    [feat] - 커스텀 로그인 필터를 SecurityConfig에 등록

--- api-server/src/main/java/com/ssafy/newstagram/api/config/SecurityConfig.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/config/SecurityConfig.java
@@ -1,16 +1,31 @@
 package com.ssafy.newstagram.api.config;
 
+import com.ssafy.newstagram.api.auth.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCrybpPasswordEncoder(){
@@ -29,6 +44,8 @@ public class SecurityConfig {
             .headers(headers -> headers
                 .frameOptions(frame -> frame.sameOrigin())  // H2 Console 등을 위해
             );
+
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
         
         return http.build();
     }
