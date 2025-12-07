b796834974d237d39b1fc8acca341ea14549284d
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-12-08 05:16:38 +0900

    [fix] - OAuth2 회원가입 시 passwordHash 값이 없어서 발생하는 에러 해결

--- api-server/src/main/java/com/ssafy/newstagram/api/auth/model/service/CustomOAuth2UserService.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/auth/model/service/CustomOAuth2UserService.java
@@ -6,17 +6,22 @@ import com.ssafy.newstagram.api.auth.model.dto.OAuth2Response;
 import com.ssafy.newstagram.api.users.repository.UserRepository;
 import com.ssafy.newstagram.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -40,8 +45,13 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         User existData = userRepository.findByLoginTypeAndProviderId("GOOGLE", oAuth2Response.getProviderId());
 
         if(existData == null){ // 최초 로그인하는 경우 => 회원가입 진행
+            String dummyPassword = UUID.randomUUID().toString();
+            PasswordEncoder encoder = new BCryptPasswordEncoder(); // DI 문제 생겨서 일단 직접 생성함
+            String encoded = encoder.encode(dummyPassword);
+
             User user = User.builder()
                 .email(oAuth2Response.getEmail())
+                .passwordHash(encoded) // 암호화된 랜덤 더미 비밀번호
                 .nickname(oAuth2Response.getName())
                 .loginType("GOOGLE")
                 .providerId(oAuth2Response.getProviderId())
