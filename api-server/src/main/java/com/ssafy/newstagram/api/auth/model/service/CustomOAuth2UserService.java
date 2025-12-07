52eb02e132e7609f87375a1404852889e5385d59
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-12-08 02:03:10 +0900

    [feat] - 최초 OAuth2 로그인 시에 회원가입 진행하는 로직 추가

--- api-server/src/main/java/com/ssafy/newstagram/api/auth/model/service/CustomOAuth2UserService.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/auth/model/service/CustomOAuth2UserService.java
@@ -3,7 +3,9 @@ package com.ssafy.newstagram.api.auth.model.service;
 import com.ssafy.newstagram.api.auth.model.dto.CustomOAuth2User;
 import com.ssafy.newstagram.api.auth.model.dto.GoogleResponse;
 import com.ssafy.newstagram.api.auth.model.dto.OAuth2Response;
+import com.ssafy.newstagram.api.users.repository.UserRepository;
 import com.ssafy.newstagram.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -11,7 +13,10 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -32,13 +37,25 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
-        User user = User.builder()
+        User existData = userRepository.findByLoginTypeAndProviderId("GOOGLE", oAuth2Response.getProviderId());
+
+        if(existData == null){ // 최초 로그인하는 경우 => 회원가입 진행
+            User user = User.builder()
                 .email(oAuth2Response.getEmail())
                 .nickname(oAuth2Response.getName())
                 .loginType("GOOGLE")
                 .providerId(oAuth2Response.getProviderId())
                 .build();
 
-        return new CustomOAuth2User(user);
+            userRepository.save(user);
+
+            return new CustomOAuth2User(user);
+
+        } // todo : email 회원가입 계정과 중복되는 경우 처리
+        else{
+            // todo: 가입 이후 이메일이 변경되는 경우는 나중에 고려..
+            return new CustomOAuth2User(existData);
+        }
+
     }
 }
