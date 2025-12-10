f3827dce97c491637eae70f015af917dc9896498
hyunjo01 <hyunjo10217@gmail.com>
2025-12-10 16:03:58 +0900

    [feat] - util/period: 기간 계산기
    REALTIME: 6시간 전 ~ 현재
    DAILY: 전일 00:00:00 ~ 금일 00:00:00
    WEEKLY: 전주 월요일 00:00:00 ~ 금주 월요일 00:00:00

---
+++ rss-collector/src/main/java/com/ssafy/newstagram/rss/clustering/util/period/PeriodCalculator.java
@@ -0,0 +1,8 @@
+package com.ssafy.newstagram.rss.clustering.util.period;
+
+import java.time.LocalDateTime;
+
+public interface PeriodCalculator {
+    LocalDateTime getStart();
+    LocalDateTime getEnd();
+}
