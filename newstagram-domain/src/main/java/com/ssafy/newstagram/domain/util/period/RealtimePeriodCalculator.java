bacd135bddb44cdd46eb44e52c8b6cc24459d218
hyunjo01 <hyunjo10217@gmail.com>
2025-12-12 12:07:34 +0900

    [feat] - REALTIME 계산 rss 업데이트 변경 시간에 맞게 수정
    00시, 6시, 9시, 12시, 15시, 18시, 21시

--- newstagram-domain/src/main/java/com/ssafy/newstagram/domain/util/period/RealtimePeriodCalculator.java
+++ newstagram-domain/src/main/java/com/ssafy/newstagram/domain/util/period/RealtimePeriodCalculator.java
@@ -5,95 +5,50 @@ import java.time.LocalDateTime;
 
 public class RealtimePeriodCalculator implements PeriodCalculator {
 
+    // 기준 시간대
+    private static final int[] BLOCKS = {0, 6, 9, 12, 15, 18, 21};
+    private static final int BLOCK_SIZE = 7;
+
+    static int findEndBlockIdx(int hour) {
+        int idx = 0;
+        for (int i = 0; i < BLOCK_SIZE; i++) {
+            if (BLOCKS[i] > hour) break;
+            idx = i;
+        }
+        return idx;
+    }
+
+    static LocalDateTime findDateTimeByIdx(LocalDate date, int idx) {
+        while (idx < 0) {
+            idx += BLOCK_SIZE;
+            date = date.minusDays(1);
+        }
+        return date.atTime(BLOCKS[idx], 0);
+    }
+
     @Override
     public LocalDateTime getStart() {
         LocalDateTime now = LocalDateTime.now();
-
-        // 현재 시간이 속한 6시간 구간 인덱스 (0~3)
-        int block = now.getHour() / 6;
-
-        // 조회해야 하는 이전 구간
-        int prevBlock = block - 1;
-        LocalDate date = now.toLocalDate();
-
-        // block == 0이면 전날 마지막 구간(18~24)을 조회해야 함
-        if (prevBlock < 0) {
-            prevBlock = 3; // 18~24
-            date = date.minusDays(1);
-        }
-
-        int startHour = prevBlock * 6;
-        return date.atTime(startHour, 0);
+        int idx = findEndBlockIdx(now.getHour());
+        return findDateTimeByIdx(now.toLocalDate(), idx-1);
     }
 
     @Override
     public LocalDateTime getEnd() {
         LocalDateTime now = LocalDateTime.now();
-
-        int block = now.getHour() / 6;
-        int prevBlock = block - 1;
-        LocalDate date = now.toLocalDate();
-
-        if (prevBlock < 0) {
-            prevBlock = 3;
-            date = date.minusDays(1);
-        }
-
-        int endHour = prevBlock * 6 + 6; // 6시간 뒤
-        if (endHour == 24) endHour = 0;
-
-        // endHour가 0이면 다음날 00시
-        if (endHour == 0) {
-            return date.plusDays(1).atStartOfDay();
-        }
-
-        return date.atTime(endHour, 0);
+        int idx = findEndBlockIdx(now.getHour());
+        return findDateTimeByIdx(now.toLocalDate(), idx);
     }
 
     @Override
     public LocalDateTime getBeforeStart() {
-        LocalDateTime now = LocalDateTime.now().minusHours(6);
-
-        // 현재 시간이 속한 6시간 구간 인덱스 (0~3)
-        int block = now.getHour() / 6;
-
-        // 조회해야 하는 이전 구간
-        int prevBlock = block - 1;
-        LocalDate date = now.toLocalDate();
-
-        // block == 0이면 전날 마지막 구간(18~24)을 조회해야 함
-        if (prevBlock < 0) {
-            prevBlock = 3; // 18~24
-            date = date.minusDays(1);
-        }
-
-        int startHour = prevBlock * 6;
-        return date.atTime(startHour, 0);
+        LocalDateTime now = LocalDateTime.now();
+        int idx = findEndBlockIdx(now.getHour());
+        return findDateTimeByIdx(now.toLocalDate(), idx-2);
     }
 
     @Override
     public LocalDateTime getBeforeEnd() {
-        LocalDateTime now = LocalDateTime.now().minusHours(6);
-
-        int block = now.getHour() / 6;
-        int prevBlock = block - 1;
-        LocalDate date = now.toLocalDate();
-
-        if (prevBlock < 0) {
-            prevBlock = 3;
-            date = date.minusDays(1);
-        }
-
-        int endHour = prevBlock * 6 + 6; // 6시간 뒤
-        if (endHour == 24) endHour = 0;
-
-        // endHour가 0이면 다음날 00시
-        if (endHour == 0) {
-            return date.plusDays(1).atStartOfDay();
-        }
-
-        return date.atTime(endHour, 0);
+        return getStart();
     }
-
-
 }
