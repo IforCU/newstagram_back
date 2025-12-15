3754534f5e85fa21a1aad4619de1aa06a6736997
hyunjo01 <hyunjo10217@gmail.com>
2025-12-15 18:18:02 +0900

    [feat] - ArticleController: 기간별 이슈 클러스터별 n개 호출

--- api-server/src/main/java/com/ssafy/newstagram/api/article/controller/ArticleController.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/article/controller/ArticleController.java
@@ -1,21 +1,39 @@
 package com.ssafy.newstagram.api.article.controller;
 
 import com.ssafy.newstagram.api.article.dto.ArticleDto;
+import com.ssafy.newstagram.api.article.dto.HotIssueSetDto;
 import com.ssafy.newstagram.api.article.service.ArticleService;
+import com.ssafy.newstagram.api.article.service.HotIssueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/article")
+@RequiredArgsConstructor
 public class ArticleController {
 
     private final ArticleService articleService;
-    public ArticleController(ArticleService articleService) {
-        this.articleService = articleService;
-    }
+    private final HotIssueService hotIssueService;
+
 
     @GetMapping("/detail/{id}")
     public ArticleDto getArticleDto(@PathVariable Long id) {
         return articleService.getArticleDto(id);
+    }
+
+    @GetMapping("/hot-issues")
+    public ResponseEntity<HotIssueSetDto> getHotIssues(
+            @RequestParam(name = "periodType") String periodType,
+            @RequestParam(name = "limitCount", defaultValue = "3") int limitCount
+    ) {
+        return ResponseEntity.ok(
+                hotIssueService.getPeriodRecommendationsTopNPerRankingByPeriodType(
+                        periodType,
+                        limitCount
+                )
+        );
     }
 
 }
